c116212cadae366dc370e094d0bd4fa75748625b
FAWRY\Abdelrahman.Khater <Abdelrahman.khater@fawry.com>
2021-09-05 10:10:38 +0200

    done with read and write

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.firebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.firebaseapp.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val database=Firebase.database
+        val ref=database.getReference("name")
+        var output="EMPTY"
+        // Read from the database
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue()
+               output=value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                output=error.message
+            }
+        })
+        binding.saveBtn.setOnClickListener {
+            if (!binding.inputEditText.text.isNullOrEmpty()){
+                ref.setValue(binding.inputEditText.text.toString())
+            }
+        }
+        binding.getBtn.setOnClickListener {
+            binding.outputTextView.text=output
+        }
+
     }
 }
